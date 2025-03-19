26016c0ebb031728b5ec233e5c54e3eebd706306
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-03-20 00:30:35 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -20,27 +20,26 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const usedMem = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
 
         // Stylish and detailed system status message
-        const status = `╭───❰ *𝐂𝐀𝐒𝐄𝐘𝐑𝐇𝐎𝐃𝐄𝐒 𝐗𝐌𝐃* ❱───➤
+        const status = `╭───❰ *Jᴏsʜᴜᴀᴍᴀᴍʙᴏᴢᴍ 𝐗𝐌𝐃* ❱───➤
 ┃ ✨ 𝗨𝗽𝘁𝗶𝗺𝗲: *${runtime(process.uptime())}*
 ┃ 💾 𝗥𝗮𝗺 𝗨𝘀𝗮𝗴𝗲: *${usedMem}MB / ${totalMem}MB*
 ┃ 🧑‍💻 𝗗𝗲𝗽𝗹𝗼𝘆𝗲𝗱 𝗢𝗻: *${platform}*
 ┃ 🔧 𝗖𝗣𝗨: *${cpuModel}*
-┃ 👨‍💻 𝗢𝘄𝗻𝗲𝗿: *mr cαѕєчrhσdєѕ*
+┃ 👨‍💻 𝗢𝘄𝗻𝗲𝗿: *mr 𝕁𝕠𝕤𝕙𝕦𝕒𝕞𝕒𝕞𝕓𝕠𝕫𝕞*
 ┃ 🧬 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: *𝟯.𝟬.𝟬 𝗕𝗘𝗧𝗔*
 ╰───────────────────────➤
-> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄᴀsᴇʏʀʜᴏᴅᴇs`;
+> ᴊᴏsʜᴋɪɴɢ01`;
 
         // Send image + caption + audio combined
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/wN6Gw0ZF/lordcasey.jpg` },  
+            image: { url: `https://i.postimg.cc/GmRpKL8j/1717623406802.jpg` },  
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363302677217436@newsletter',
-                    newsletterName: '𝐂𝐀𝐒𝐄𝐘𝐑𝐇𝐎𝐃𝐄𝐒 𝐀𝐋𝐈𝐕𝐄🍀',
+                    newsletterName: '𝕁𝕆𝕊ℍ𝕌𝔸𝕄𝔸𝕄𝔹𝕆 𝐀𝐋𝐈𝐕𝐄🍀',
                     serverMessageId: 143
                 }
             }
@@ -48,7 +47,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Attach audio within the same "quoted" message for grouping
         await conn.sendMessage(from, { 
-            audio: { url: 'https://files.catbox.moe/3au05j.m4a' },
+            audio: { url: 'https://files.catbox.moe/da6m1z.mp4' },
             mimetype: 'audio/mp4',
             ptt: true 
         }, { quoted: mek });
